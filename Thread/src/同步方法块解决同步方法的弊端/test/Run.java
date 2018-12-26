ac278d5cb06dff34725de74855de8bb0668639e6
hxllhhy <979641269@qq.com>
2018-12-26 20:51:30 +0800

    同步方法块解决同步方法的弊端

---
+++ Thread/src/同步方法块解决同步方法的弊端/test/Run.java
@@ -0,0 +1,86 @@
+package 同步方法块解决同步方法的弊端.test;
+
+class Task {
+    private String getData1;
+    private String getData2;
+    public void doLongTimeTask() {
+        try {
+            System.out.println("begin task...");
+            Thread.sleep(3000);
+            String privateGetData1 = "长时间处理任务后从远程返回的值1 threadName=" + Thread.currentThread().getName();
+
+            String privateGetData2 = "长时间处理任务后从远程返回的值2 threadName=" + Thread.currentThread().getName();
+            synchronized (this) {
+                getData1 = privateGetData1;
+                getData2 = privateGetData2;
+            }
+            System.out.println(getData1);
+            System.out.println(getData2);
+            System.out.println("end task");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class CommonUtils {
+    public static long beginTime1;
+    public static long endTime1;
+    public static long beginTime2;
+    public static long endTime2;
+}
+
+class MyThread1 extends Thread {
+    private Task task;
+    public MyThread1(Task task) {
+        super();
+        this.task = task;
+    }
+    @Override
+    public void run() {
+        super.run();
+        CommonUtils.beginTime1 = System.currentTimeMillis();
+        task.doLongTimeTask();
+        CommonUtils.endTime1 = System.currentTimeMillis();
+    }
+}
+
+class MyThread2 extends Thread {
+    private Task task;
+    public MyThread2(Task task) {
+        super();
+        this.task = task;
+    }
+    @Override
+    public void run() {
+        super.run();
+        CommonUtils.beginTime2 = System.currentTimeMillis();
+        task.doLongTimeTask();
+        CommonUtils.endTime2 = System.currentTimeMillis();
+    }
+}
+
+public class Run {
+    public static void main(String[] args) {
+        Task task = new Task();
+        MyThread1 t1 = new MyThread1(task);
+        t1.start();
+        MyThread2 t2 = new MyThread2(task);
+        t2.start();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        long beginTime = CommonUtils.beginTime1;
+        if(CommonUtils.beginTime2 < CommonUtils.beginTime1) {
+            beginTime = CommonUtils.beginTime2;
+        }
+        long endTime = CommonUtils.endTime1;
+        if(CommonUtils.beginTime2 > CommonUtils.endTime1) {
+            endTime = CommonUtils.endTime2;
+        }
+        System.out.println("耗时:" + ((endTime - beginTime)/1000));
+    }
+
+}
